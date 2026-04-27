fe8c6bfbdd22aab555a8b79649e4efe650e2afb8
2shoneycom <2shoneycom@gmail.com>
2026-04-27 20:29:52 +0900

    feat(admin-event): [#TK-138] 관리자 이벤트 관리 구현 #3
    작업 내용:
    -AdminEventController 엔드포인트 코드 작성

--- backend/src/main/java/com/ticketmaster/backend/admin/event/controller/AdminEventController.java
+++ backend/src/main/java/com/ticketmaster/backend/admin/event/controller/AdminEventController.java
@@ -1,9 +1,72 @@
 package com.ticketmaster.backend.admin.event.controller;
 
-import org.springframework.stereotype.Controller;
+import com.ticketmaster.backend.admin.event.dto.request.AdminEventCreateRequest;
+import com.ticketmaster.backend.admin.event.dto.request.AdminEventUpdateRequest;
+import com.ticketmaster.backend.admin.event.dto.response.AdminEventDetailResponse;
+import com.ticketmaster.backend.admin.event.dto.response.AdminEventListResponse;
+import com.ticketmaster.backend.admin.event.dto.response.AdminEventResponse;
+import com.ticketmaster.backend.admin.event.service.AdminEventService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/admin/events")
+@PreAuthorize("hasRole('ADMIN')")
+@RequiredArgsConstructor
 public class AdminEventController {
+    private final AdminEventService eventService;
 
-    
+    /**
+     * 이벤트 등록
+     */
+    @PostMapping
+    public ResponseEntity<AdminEventResponse> createEvent(@Valid @RequestBody AdminEventCreateRequest request) {
+        AdminEventResponse response = eventService.createEvent(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    /**
+     * 이벤트 목록 조회 (간단한 정보 리스트)
+     */
+    @GetMapping
+    public ResponseEntity<Page<AdminEventListResponse>> getEventList(Pageable pageable) {
+        return ResponseEntity.ok(eventService.getEventList(pageable));
+    }
+
+    /**
+     * 이벤트 상세 조회 (상세 정보 리스트)
+     */
+    @GetMapping("/{eventId}")
+    public ResponseEntity<AdminEventDetailResponse> getEventDetail(@PathVariable Long eventId) {
+        return ResponseEntity.ok(eventService.getEventDetail(eventId));
+    }
+
+    /**
+     * 이벤트 수정
+     */
+    @PatchMapping("/{eventId}")
+    public ResponseEntity<AdminEventResponse> updateEvent(
+            @PathVariable Long eventId,
+            AdminEventUpdateRequest request) {
+        AdminEventResponse response = eventService.updateEvent(eventId, request);
+
+        return ResponseEntity.ok(response);
+    }
+
+    /**
+     * 이벤트 삭제
+     */
+    @DeleteMapping("/admin/events/{eventId}")
+    public ResponseEntity<Void> deleteEvent(@PathVariable Long eventId) {
+        eventService.deleteEvent(eventId);
+
+        return ResponseEntity.noContent().build();
+    }
 }
