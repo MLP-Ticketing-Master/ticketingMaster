4f348f2666d072a87ffbc24f3212215468bb865c
Masozravec09 <jiwon0922@gmail.com>
2026-06-05 11:05:17 +0900

    refactor: 반응형 UI 개선 (#96)
    * refactor:배너 수정 및 이미지 변경
    
    * refactor:모바일 반응형 이미지 수정, 슬라이드 넘기기 버튼 추가
    
    ---------
    
    Co-authored-by: SeungHeon Lee <142134459+2shoneycom@users.noreply.github.com>

--- frontend/src/components/event/HeroSection.tsx
+++ frontend/src/components/event/HeroSection.tsx
@@ -1,28 +1,49 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-import banner1 from "@/assets/LCKbanner.png";
-import banner2 from "@/assets/VALbanner.png";
-import banner3 from "@/assets/PUBGbanner.png";
-import banner4 from "@/assets/WATCHbanner.png";
+import desktopLck from "@/assets/LCKbanner.png";
+import desktopVal from "@/assets/VALbanner.png";
+import desktopPubg from "@/assets/PUBGbanner.png";
+import desktopWatch from "@/assets/WATCHbanner.png";
+import mobileLck from "@/assets/LCKmobile.png";
+import mobileVal from "@/assets/VALmobile.png";
+import mobilePubg from "@/assets/PUBGmobile.png";
+import mobileWatch from "@/assets/WATCHmobile.png";
 
 const banners = [
-  {image:banner1,
+  {image:desktopLck,
+    mobileImage: mobileLck,
     link: "/events/3952",
    },
-  {image:banner2,
+  {image:desktopVal,
+    mobileImage: mobileVal,
     link: "/events/3954",
    },
-  {image:banner3,
+  {image:desktopPubg,
+    mobileImage: mobilePubg,
     link: "/events/3957",
    },
-  {image:banner4,
+  {image:desktopWatch,
+    mobileImage: mobileWatch,
     link: "/events/3955",
    },
   ];
 
+
 export function HeroSection() {
   const [current, setCurrent] = useState(0);
+
+  const prevSlide = () => {
+    setCurrent((prev) =>
+      prev === 0 ? banners.length - 1 : prev - 1
+    );
+  };
+
+  const nextSlide = () => {
+    setCurrent((prev) =>
+      (prev + 1) % banners.length
+    );
+  };
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -34,7 +55,10 @@ export function HeroSection() {
 
   return (
     <section className="flex justify-center px-6 py-6">
-      <div className="relative mx-auto h-[400px] max-w-7x1 w-[1240px] overflow-hidden rounded-3xl shadow-xl ">
+      <div className="relative group mx-auto
+            h-[300px] md:h-[400px]
+            w-full max-w-7xl
+            overflow-hidden rounded-3xl">
         <div
             className="flex h-full transition-transform duration-700 ease-in-out" 
             style={{
@@ -48,16 +72,18 @@ export function HeroSection() {
               to={banner.link}
               className="relative h-full min-w-full block"
             >
-          <div
-            key={index}
-            className="relative h-full min-w-full"
-          >
+          <div className="relative h-full min-w-full">
+            <picture>
+            <source
+              media="(max-width: 768px)"
+              srcSet={banner.mobileImage}
+            />
             <img
               src={banner.image}
               alt={`banner-${index}`}
-              className="h-full w-full object-cover scale-100"
+              className="w-full h-full object-cover"
             />
-
+            </picture>
             <div className="absolute inset-0 bg-black/20 hover:bg-black/10 transition" />
             
           </div>
@@ -80,6 +106,36 @@ export function HeroSection() {
           />
         ))}
       </div>
+
+      <button
+      onClick={prevSlide}
+      className="
+        absolute left-4 top-1/2 -translate-y-1/2 z-20
+        h-12 w-12 rounded-full
+        bg-black/30 backdrop-blur-sm
+        text-white
+        opacity-0
+        group-hover:opacity-100
+        transition-all duration-300
+      "
+    >
+      ‹
+    </button>
+
+    <button
+      onClick={nextSlide}
+      className="
+        absolute right-4 top-1/2 -translate-y-1/2 z-20
+        h-12 w-12 rounded-full
+        bg-black/30 backdrop-blur-sm
+        text-white
+        opacity-0
+        group-hover:opacity-100
+        transition-all duration-300
+      "
+    >
+      ›
+    </button>
     </div>
     </section>
   );
